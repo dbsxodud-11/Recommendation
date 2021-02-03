6555efa18a8103a6813e19639bf1683bd689749a
dbsxodud11 <99yty@kaist.ac.kr>
2021-02-03 16:55:01 +0900

    ALS revised

--- ALS.py
+++ ALS.py
@@ -1,40 +1,45 @@
 import numpy as np
 import pandas as pd
 from scipy import sparse
+import matplotlib.pyplot as plt
 
 class ALS :
 
-    def __init__(self, path, n_factor, alpha, reg, k) :
+    def __init__(self, path, n_factor, alpha, reg, seed) :
 
         self.path = path
-        self.dataset_name = "MovieLens-1M"
+        self.dataset_name = "MovieLens-100K"
         self.f = n_factor
         self.alpha = alpha
         self.reg = reg
-        self.k = k
+        self.seed = seed
 
     def run(self) :
 
         self.load_data()
         self.train()
-        self.evaluate()
+        performance = self.evaluate()
+        return performance
 
     def load_data(self) :
 
         print("Dataset %s" %self.dataset_name)
-        self.n_users = 6040
-        self.n_items = 3952
-        with open(self.path + "ratings.dat") as f :
+        print("# Factor %d" %self.f)
+        np.random.seed(self.seed)
+        self.n_users = 943
+        self.n_items = 1682
+        with open(self.path + "u.data") as f :
             self.matrix = np.zeros((self.n_users, self.n_items))       
             lines = f.readlines()
             for line in lines :
-                row, col, val = map(int, line.strip().split("::")[:-1])
+                row, col, val = map(int, line.strip().split("\t")[:-1])
                 self.matrix[row-1,col-1] = val
 
         self.data_splitter = np.random.binomial(n=1, p=0.8, size=self.matrix.shape)
         
         self.train_matrix = np.multiply(self.matrix, self.data_splitter)
-        self.test_matrix = np.multiply(self.matrix, self.data_splitter^1)
+        self.test_matrix = np.where(np.multiply(self.matrix, self.data_splitter^1) >= 5, 5, 0)
+
 
         self.train_pref = np.where(self.train_matrix>0, 1, 0)
         self.train_conf = 1 + self.alpha * self.train_matrix
@@ -44,8 +49,11 @@ class ALS :
         self.X = np.random.normal(scale=1.0/self.f, size=(self.n_users, self.f))
         self.Y = np.random.normal(scale=1.0/self.f, size=(self.n_items, self.f))
         for episode in range(10) :
-            self.X = self.train_by_ALS(self.Y, axis=0)
-            self.Y = self.train_by_ALS(self.X, axis=1)
+            self.train_by_ALS(self.Y, axis=0)
+            self.train_by_ALS(self.X, axis=1)
+            # Calculate Loss
+            loss = np.sum(np.multiply(self.train_conf, np.square(self.train_pref-np.matmul(self.X, self.Y.T)))) + self.reg * (np.sum(np.square(self.X)) + np.sum(np.square(self.Y)))
+            print("Episode :  %d   Loss : %f" %(episode, loss))
 
     def train_by_ALS(self, W, axis) :
         n = self.matrix.shape[axis]
@@ -57,16 +65,36 @@ class ALS :
         for i in range(self.matrix.shape[axis]) :
             if axis == 0 :
                 C_W = np.diag(self.train_conf[i,:])
-                A = WTW + lambda_I + np.matmul(W.T, np.matmul(C_W - np.identity(m), self.train_pref[i,:]))
+                A = WTW + lambda_I + np.matmul(W.T, np.matmul(C_W - np.identity(m), W))
                 y = np.matmul(np.matmul(W.T, C_W), self.train_pref[i,:])
+                self.X[i] = np.linalg.solve(A, y)
             else :
                 C_W = np.diag(self.train_conf[:,i])
-                A = WTW + lambda_I + np.matmul(W.T, np.matmul(C_W - np.identity(n), self.train_conf[:,i]))
-                y = np.matmul(np.matmul(W.T, C_W), self.train_prf[:,i])
-            W[i] = np.linalg.solve(A, y)
-        print(W.shape)
-        return W
+                A = WTW + lambda_I + np.matmul(W.T, np.matmul(C_W - np.identity(m), W))
+                y = np.matmul(np.matmul(W.T, C_W), self.train_pref[:,i])
+                self.Y[i] = np.linalg.solve(A, y)
+
+    def evaluate(self) :
+
+        pred_pref = np.matmul(self.X, self.Y.T)
+        ranking = np.argsort(-pred_pref, axis=1)
+
+        percentile_ranking = np.sum(np.multiply(ranking.argsort(), self.test_matrix)) / np.sum(self.test_matrix)
+        percentile_ranking /= self.n_items
+        return percentile_ranking
+
 
 if __name__ == "__main__" :
-    als = ALS("./ml-1m/", n_factor=10, alpha=40, reg=100, k=100)
-    als.run()
+    performance_list = []
+    n_factors = [10, 20, 40, 80, 160]
+    for n_factor in n_factors :
+        als = ALS("./ml-100k/", n_factor=n_factor, alpha=40, reg=1.0/n_factor, seed=10)
+        performance = als.run()
+        performance_list.append(performance)
+
+    plt.plot(n_factors, performance_list, linestyle = "dashed", label = "ALS", color = "black")
+    plt.title("Percentile Ranking of ALS")
+    plt.xlabel("# factors")
+    plt.ylabel("Expected Percentile Ranking")
+    plt.legend()
+    plt.savefig("./data/ALS_performance.png")
--- sandbox.py
+++ sandbox.py
@@ -1,36 +1,74 @@
 import numpy as np
+from scipy import sparse
 import torch
 import random
-# action = np.random.normal(size=(10, 5, 4))
-actions = []
-for i in range(5) :
-    action = []
-    for j in range(4) :
-        action.append(np.random.normal(size=3))
-    actions.append(np.array(action))
-actions = np.array(actions)
-print(actions)
-actions = np.transpose(actions, (1, 0, 2))
-print(actions)
-actions = torch.from_numpy(actions).float().transpose(0, 1)
-print(actions)
+# # action = np.random.normal(size=(10, 5, 4))
+# actions = []
+# for i in range(5) :
+#     action = []
+#     for j in range(4) :
+#         action.append(np.random.normal(size=3))
+#     actions.append(np.array(action))
+# actions = np.array(actions)
+# print(actions)
+# actions = np.transpose(actions, (1, 0, 2))
+# print(actions)
+# actions = torch.from_numpy(actions).float().transpose(0, 1)
+# print(actions)
 
 
+# # action = []
+# # for i in range(4) :
+# #     action.append(np.random.normal(size=4))
+# # action = np.array(action)
+# # print(action)
+# # print()
+# # print(np.transpose(action))
+
+
+# policy = np.array([0.1, 0.2, 0.3, 0.1])
 # action = []
-# for i in range(4) :
-#     action.append(np.random.normal(size=4))
-# action = np.array(action)
+# for k in range(10000) :
+#     action.append(random.choices([i for i in range(4)], weights=policy)[0])
 # print(action)
+# print(len(list(filter(lambda x: x==0, action))))
+# print(len(list(filter(lambda x: x==1, action))))
+# print(len(list(filter(lambda x: x==2, action))))
+# print(len(list(filter(lambda x: x==3, action))))
+
+x = np.array([[1, 0, 0],[5, 2, 0],[0, 0, 3],[0, 0, 1]])
+x = sparse.csr_matrix(x)
+row = x
+col = x.transpose()
+# print(row)
 # print()
-# print(np.transpose(action))
+# print(col)
+# print(col.tocsr())
+
+x1 = np.random.normal(size=(3, 4))
+# y = np.repeat(np.expand_dims(x1, axis=0), 3, axis=0)
+# print(x1)
+# print(y)
+# print(x1)
+# print(row.indices)
+# print(row.shape)
+# print(col.indices)
+# print(col.shape)
+# lookup = x1[row.indices]
+# print(lookup)
+# print(x1[col.indices])
+# expanded_lookup = np.expand_dims(lookup, axis=1)
+# print(expanded_lookup)
+# print(np.transpose(expanded_lookup, axes=[0,2,1]))
+# print(np.matmul(np.transpose(expanded_lookup, axes=[0,2,1]), expanded_lookup))
 
 
-policy = np.array([0.1, 0.2, 0.3, 0.1])
-action = []
-for k in range(10000) :
-    action.append(random.choices([i for i in range(4)], weights=policy)[0])
-print(action)
-print(len(list(filter(lambda x: x==0, action))))
-print(len(list(filter(lambda x: x==1, action))))
-print(len(list(filter(lambda x: x==2, action))))
-print(len(list(filter(lambda x: x==3, action))))
+a = np.array([[0,1,0,0],[0,0,1,1],[0,0,1,0]])
+a = torch.tensor(a)
+print(a.view(3, 1, 4))
+# a = sparse.csr_matrix(a)
+# print(a.indices)
+# print(a.indptr)
+
+a = np.array([[1],[3],[5]])
+print(np.sum(a, axis=1))
